9df647c351423a1fa99a7816fef0ef40f8a1392a
Christoph Guttandin <chrisguttandin@media-codings.com>
2013-10-02 14:07:42 +0200

    create first meaningful e2e test

--- test/e2e/textaposer.js
+++ test/e2e/textaposer.js
@@ -1,12 +1,12 @@
 'use strict';
 
-describe('nameme', function () {
+describe('textaposer', function () {
 
     beforeEach(function() {
         browser().navigateTo('/');
     });
 
-    it('should attach a list of awesomeThings to the scope', function () {
+    it('should change the length binding', function () {
         expect(input('length').val()).toEqual('3');
         input('length').enter(5);
         //expect(binding('length')).toEqual(5);
